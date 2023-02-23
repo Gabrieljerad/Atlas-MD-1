d38c81b33a276ac23e0ae48604f0c9a72e3f2714
ʀᴇᴅᴢᴇᴏꭗ <105351945+REDZEOX@users.noreply.github.com>
2023-02-23 11:12:27 +0530

    🕯️✨ʀᴇᴅᴢᴇᴏꭗ✨🕯️

--- Commands/Economy/bank.js
+++ Commands/Economy/bank.js
@@ -3,9 +3,7 @@ require("../../config.js");
 require("../../Core.js");
 const { mku, mk } = require("../../Database/dataschema.js");
 const fs = require("fs");
-const config = require('../../config');
-const eco = require('discord-mongoose-economy')
-const ty = eco.connect(config.mongodb);
+
 
 module.exports = { 
 
@@ -17,7 +15,7 @@ module.exports = {
     start: async ( 
         Miku, 
       m, 
-      { text, prefix, isBotAdmin, isAdmin, mentionByTag, pushName, isCreator} 
+      { text, prefix, isBotAdmin, isAdmin, mentionByTag, pushName, isCreator,eco,ty} 
     ) => {
         const pushname = m.pushName //|| 'NO name'
         const user = m.sender
